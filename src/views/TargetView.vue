91fb48e1f76ef494f206db7800cda70c4b81ebb3
Azreil-OFD <fantom2413@gmail.com>
2024-07-19 17:11:11 +0500

    fix

---
+++ src/views/TargetView.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <a href="#" @click="router.push('/menu')"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Table</a>
+
+
+    <a href="#" @click="router.push('/view')"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">TV
+    </a>
+
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+</script>
