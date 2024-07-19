cdf76d1035f0a120932d04318ff0b7541abda9e5
Azreil-OFD <fantom2413@gmail.com>
2024-07-19 17:05:33 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/tablet_ui/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
